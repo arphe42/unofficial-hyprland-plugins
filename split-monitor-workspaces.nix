d2b7e39be0dbd2323068cefeba89ae0b95649d61
arphe42 <arphe42@gmail.com>
2023-08-09 00:13:27 -0400

    test

--- split-monitor-workspaces.nix
+++ split-monitor-workspaces.nix
@@ -1,17 +1,25 @@
 {
   lib,
   stdenv,
+  fetchFromGitHub,
   clang,
   hyprland,
 }:
 stdenv.mkDerivation {
   pname = "split-monitor-workspaces";
   version = "1.0.0";
-  src = ./split-monitor-workspaces;
+  src = fetchFromGitHub {
+    owner = "Duckonaut";
+    repo = " split-monitor-workspaces";
+    rev = "1.0.0";
+    sha256 = "sha256-8IH0WTPSaAundhYh4l7gQR66nyT38H4DstRTm+Xh+Z8=";
+  };
 
   inherit (hyprland) nativeBuildInputs;
 
   buildInputs = [hyprland] ++ hyprland.buildInputs;
+
+  buildPhase = "make all";
 
   meta = with lib; {
     homepage = "https://github.com/Duckonaut/split-monitor-workspaces";
