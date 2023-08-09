cae86a197a4230617f08329f48b2dccd8840933a
arphe42 <arphe42@gmail.com>
2023-08-08 23:03:39 -0400

    Split monitor workspaces

---
+++ split-monitor-workspaces.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  stdenv,
+  hyprland,
+}:
+stdenv.mkDerivation {
+  pname = "split-monitor-workspaces";
+  version = "1.0.0";
+  src = ./split-monitor-workspaces;
+
+  inherit (hyprland) nativeBuildInputs;
+
+  buildInputs = [hyprland] ++ hyprland.buildInputs;
+
+  meta = with lib; {
+    homepage = "https://github.com/Duckonaut/split-monitor-workspaces";
+    description = "Split monitor workspaces";
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+  };
+}
