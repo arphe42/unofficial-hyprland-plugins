edd3ee8e7e9a3be4b9fb641c518f1b3829ce5a14
arphe42 <arphe42@gmail.com>
2023-08-08 22:51:09 -0400

    add the flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "unofficial Hyprland Plugins";
+
+  inputs.hyprland.url = "github:hyprwm/Hyprland";
+
+  outputs = { 
+    self,
+    nixpkgs
+  }: let
+    inherit (hyprland.inputs) nixpkgs;
+    withPkgsFor = fn: nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (system: fn system nixpkgs.legacyPackages.${system});
+  in {
+    packages = withPkgsFor (system: pkgs: {
+      split-monitor-workspaces = pkgs.callPackage ./split-monitor-workspaces {inherit (hyprland.packages.${system}) hyprland;};
+    });
+
+    devShells = withPkgsFor (system: pkgs: {
+      default = pkgs.mkshell {
+        name = "unofficial-hyprland-plugins";
+        buildInputs = [hyprland.packages.${system}.hyprland];
+        inputsFrom = [hyprland.packages.${system}.hyprland];
+      };
+    });
+  };
+}
